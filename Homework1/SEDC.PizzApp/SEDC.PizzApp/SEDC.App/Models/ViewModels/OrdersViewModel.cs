3b374b5af07e78fbda03459a2627c5c1bd699291
KaterinaJangelovska <jangelovska0405@gmail.com>
2019-06-06 10:10:26 +0200

    homework

---
+++ Homework1/SEDC.PizzApp/SEDC.PizzApp/SEDC.App/Models/ViewModels/OrdersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.App.Models.ViewModels
+{
+    public class OrdersViewModel
+    {
+        public string FirstPizza { get; set; }
+        public int NumberOfOrders { get; set; }
+        public string FirstPersonName { get; set; }
+        public double Price { get; set; }
+        
+    }
+}
