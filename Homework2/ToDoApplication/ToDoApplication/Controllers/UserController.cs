72b4df530f3dfd7ea4cf0c94954c2b67c59aae7f
KaterinaJangelovska <jangelovska0405@gmail.com>
2019-06-10 14:45:07 +0200

    Click me

---
+++ Homework2/ToDoApplication/ToDoApplication/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApplication.Models.DomainModels;
+using Type = ToDoApplication.Models.DomainModels.Type;
+
+namespace ToDoApplication.Controllers
+{
+    [Route("User/[action]")]
+    public class UserController : Controller
+    {
+        private List<User> _usersDb;
+        private List<Tasks> _tasksDb;
+
+
+        public UserController()
+        {
+            _usersDb = new List<User>()
+            {
+                new User()
+                {
+                FirstName = "Katerina",
+                LastName = "Jangelovska",
+                Age = 25,
+                AverageFreeTime = 6,
+                ToDoTasks = _tasksDb
+                },
+                new User()
+                {
+                FirstName = "Petko",
+                LastName = "Petkovski",
+                Age = 30,
+                AverageFreeTime = 8,
+                ToDoTasks = _tasksDb
+                },
+
+            };
+
+            _tasksDb = new List<Tasks>()
+            {
+                new Tasks
+                {
+                    Title = "Fitness",
+                    Descripton = "Leg day",
+                    Importance = Importance.mediumImportance,
+                    Status = Status.NotDone,
+                    Type = Type.Hobby,
+
+                },
+
+                new Tasks
+                {
+                    Title = "sedcHomewrok",
+                    Descripton = "ToDoApp",
+                    Importance = Importance.Important,
+                    Status = Status.Done,
+                    Type = Type.Personal,
+
+                }
+            };
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult InProgress()
+        {
+            return View();
+        }
+
+        public IActionResult NotDone()
+        {
+            User katerina = _usersDb[0];
+            return View(katerina);
+        }
+
+        public IActionResult Done()
+        {
+            return View();
+        }
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+    }
+}
---
+++ Homework2/ToDoApplication/ToDoApplication/Models/DomainModels/Tasks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApplication.Models.DomainModels
+{
+    public class Tasks
+    {
+        public string Title { get; set; }
+        public string Descripton { get; set; }
+        //Dictionary<int, string> Importance = new Dictionary<int, string>();
+        public Status Status { get; set; }
+        public Type Type { get; set; }
+        public List<SubTasks> SubTasks { get; set; }
+        public Importance Importance { get; set; }
+    }
+
+    public enum Status
+    {
+        NotDone,
+        InProgress,
+        Done
+    }
+
+    public enum Type
+    {
+        Work,
+        Personal,
+        Hobby
+    }
+
+    public enum Importance
+    {
+        Important = 1,
+        mediumImportance = 2,
+        notImportant = 3
+    }
+
+
+}
